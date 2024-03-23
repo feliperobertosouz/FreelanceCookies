ef0733b60909bed80246a4f5cbb626c3c391a6e1
Felipe <feliperobertosouz@gmail.com>
2024-03-23 18:01:06 -0300

    start project

---
+++ src/main/java/com/devcookies/freelancecookies/FreelancecookiesApplication.java
@@ -0,0 +1,13 @@
+package com.devcookies.freelancecookies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancecookiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancecookiesApplication.class, args);
+	}
+
+}
