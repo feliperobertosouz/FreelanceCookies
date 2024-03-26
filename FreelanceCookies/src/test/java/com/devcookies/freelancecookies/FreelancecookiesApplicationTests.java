4d5f046197f74e79e8e29185cdad16e2f331d44c
Gibinister <48371980+Gibinister@users.noreply.github.com>
2024-03-26 00:54:40 -0300

    Add files via upload

---
+++ FreelanceCookies/src/test/java/com/devcookies/freelancecookies/FreelancecookiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devcookies.freelancecookies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancecookiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
