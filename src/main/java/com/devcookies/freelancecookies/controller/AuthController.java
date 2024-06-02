68f2c60602c54479a3b50052ad666fb22bdebd6d
Felipe <feliperobertosouz@gmail.com>
2024-06-01 23:13:43 -0300

    added cors and LoginresponseDTO

--- src/main/java/com/devcookies/freelancecookies/controller/AuthController.java
+++ src/main/java/com/devcookies/freelancecookies/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.devcookies.freelancecookies.controller;
 
 import com.devcookies.freelancecookies.dto.LoginDTO;
+import com.devcookies.freelancecookies.dto.LoginResponseDTO;
 import com.devcookies.freelancecookies.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,19 +9,20 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin("*")
 public class AuthController {
 
     @Autowired
     private AuthService authService;
 
     @PostMapping("/login")
-    public ResponseEntity<LoginDTO> login(@RequestBody LoginDTO login) {
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginDTO login) {
         try {
             boolean isAuthenticated = authService.authenticate(login);
             if (isAuthenticated) {
-                return ResponseEntity.ok(login);
+                return ResponseEntity.ok(new LoginResponseDTO("Logado"));
             } else {
-                return ResponseEntity.status(401).body(null);
+                return ResponseEntity.status(401).body(new LoginResponseDTO("Alguma coisa deu errado"));
             }
         } catch (Exception e) {
             return ResponseEntity.status(500).body(null);
